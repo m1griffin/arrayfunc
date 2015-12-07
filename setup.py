05af3adaf5e1d079d610ab2201e17098c8ed78fa
M. Griffin <m12.griffin@gmail.com>
2015-12-06 22:45:15 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,62 +1,53 @@
 #!/usr/bin/env python3
 
-# This is not working yet!
-
 from distutils.core import setup, Extension
 
 extensions = [
-('aall', ['aall.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('aany', ['aany.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('afilter', ['afilter.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('amapi', ['amapi.c', 'amap_common.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('amap', ['amap.c', 'amap_common.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('amax', ['amax.c', 'arrayfunc.c', 'arrayerrs.c']),
-('amin', ['amin.c', 'arrayfunc.c', 'arrayerrs.c']),
-('arrayguardbands', ['arrayguardbands.c', 'arrayerrs.c']),
-('arraylimits', ['arraylimits.c']),
-('asum', ['asum.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('compress', ['compress.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('convert', ['convert.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('count', ['count.c', 'arrayfunc.c', 'arrayerrs.c']),
-('cycle', ['cycle.c', 'arrayfunc.c', 'arrayerrs.c']),
-('dropwhile', ['dropwhile.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('findindex', ['findindex.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('findindices', ['findindices.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('repeat', ['repeat.c', 'arrayfunc.c', 'arrayerrs.c']),
-('starmapi', ['starmapi.c', 'starmap_common.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('starmap', ['starmap.c', 'starmap_common.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
-('takewhile', ['takewhile.c', 'arrayfunc.c', 'arithcalcs.c', 'arrayerrs.c']),
+('aall', ['src/aall.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('aany', ['src/aany.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('afilter', ['src/afilter.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('amapi', ['src/amapi.c', 'src/amap_common.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('amap', ['src/amap.c', 'src/amap_common.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('amax', ['src/amax.c', 'src/arrayfunc.c', 'src/arrayerrs.c']),
+('amin', ['src/amin.c', 'src/arrayfunc.c', 'src/arrayerrs.c']),
+('arrayguardbands', ['src/arrayguardbands.c', 'src/arrayerrs.c']),
+('arraylimits', ['src/arraylimits.c']),
+('asum', ['src/asum.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('compress', ['src/compress.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('convert', ['src/convert.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('count', ['src/count.c', 'src/arrayfunc.c', 'src/arrayerrs.c']),
+('cycle', ['src/cycle.c', 'src/arrayfunc.c', 'src/arrayerrs.c']),
+('dropwhile', ['src/dropwhile.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('findindex', ['src/findindex.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('findindices', ['src/findindices.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('repeat', ['src/repeat.c', 'src/arrayfunc.c', 'src/arrayerrs.c']),
+('starmapi', ['src/starmapi.c', 'src/starmap_common.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('starmap', ['src/starmap.c', 'src/starmap_common.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
+('takewhile', ['src/takewhile.c', 'src/arrayfunc.c', 'src/arithcalcs.c', 'src/arrayerrs.c']),
 ]
-
-for modname, modext in extensions:
-	setup(name = modname, version='0.0', ext_modules = [Extension(modname, ['src/%s' % x for x in modext])])
 
 
 with open('README.rst') as longdescdata:
     long_description = longdescdata.read()
 
-setup(name = 'ArrayFunc',
-      version = '0.9.1rc1',
-      description = 'Fast array processing functions',
-      long_description = long_description,
-      url = 'https://github.com/arrayfunc/arrayfunc',
-      author = 'M Griffin',
-      author_email = 'm12.griffin@gmail.com',
-      license = 'Apache License V2.0',
-      classifiers = [
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Topic :: Scientific/Engineering :: Mathematics',
-          'License :: OSI Approved :: Apache License V2.0',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          ],
-      keywords = 'mathematical array functions',
-      packages=find_packages(exclude=['benchmarks', 'codegen', 'unittests']
-      ),
 
-
-
-
-
+setup(name = 'arrayfunc', 
+	version = '0.9.1rc1',
+	description = 'Fast array processing functions',
+	long_description = long_description,
+	url = 'https://github.com/arrayfunc/arrayfunc',
+	author = 'M Griffin',
+	author_email = 'm12.griffin@gmail.com',
+	license = 'Apache License V2.0',
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'Topic :: Scientific/Engineering :: Mathematics',
+		'License :: OSI Approved :: Apache License V2.0',
+		'Programming Language :: Python :: 3.4',
+		],
+	keywords = 'mathematical array functions',
+	ext_package='arrayfunc',
+	ext_modules = [Extension(x, y) for x,y in extensions],
+	packages=['arrayfunc'])
 
