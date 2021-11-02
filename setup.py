df57097699c25d3e949e1ade307ed61eaa5728c2
M. Griffin <m12.griffin@gmail.com>
2021-11-02 11:52:40 -0400

    Version 8.0.2 - Technical bump to version number to include update information.

--- setup.py
+++ setup.py
@@ -199,7 +199,7 @@ with open('README.rst') as longdescdata:
 
 
 setup(name = 'arrayfunc', 
-	version = '8.0.0',
+	version = '8.0.2',
 	description = 'Fast array processing functions',
 	long_description = long_description,
 	url = 'https://github.com/m1griffin/arrayfunc',
